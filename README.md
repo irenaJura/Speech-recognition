4d38a0c1382ff31f92046cca58c47dc87a739801
Irena Jurasek <irenaskydive@gmail.com>
2020-01-15 16:01:16 +0100

    add speak function

--- README.md
+++ README.md
@@ -9,3 +9,5 @@ Then I set up the logic to create and append p elements for the incoming text.
 Next I added an event listener for results that gives us the result transcript in a string, and if the result is final (you stop speaking), it creates a new paragraph.
 
 Finally, I had to add another event listener (end), which will start the speech recognition again and output the text.
+
+As an additional task, I instantiated speechSynthesis on a window and a new SpeechSynthesisUtterance instance which will enable the speech service to reply. There are 3 possible questions that you may ask: What is the time, what is today's date, and what is the weather in [city].
--- scripts.js
+++ scripts.js
@@ -5,6 +5,48 @@ window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecogn
 const recognition = new SpeechRecognition();
 recognition.interimResults = true; // interim results are results that are not yet final
 
+// instantiate speech synthesis
+const synth = window.speechSynthesis;
+
+// create function speak to call when we want an answer
+// SpeechSynthesisUtterance represents a speech request
+const speak = (action) => {
+    utterThis = new SpeechSynthesisUtterance(action());
+    synth.speak(utterThis);
+  };
+
+const getTime = () => {
+    const time = new Date(Date.now());
+    return `the time is ${time.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true })}`
+  };
+
+const getDate = () => {
+    const time = new Date(Date.now());
+    return `today is ${time.toDateString()}`;
+  };
+
+  const getTheWeather = (speech) => {
+        let city;
+        if(speech.split(' ')[5] && speech.split(' ')[6]) {
+            city = speech.split(' ')[5] + ' ' + speech.split(' ')[6];
+        } else {
+            city = speech.split(' ')[5];
+        }
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=58b6f7c78582bffab3936dac99c31b25&units=metric`) 
+    .then(function(response){
+      return response.json();
+    })
+    .then(function(weather){
+      if (weather.cod === '404') {
+        utterThis = new SpeechSynthesisUtterance(`I cannot find the weather for ${speech.split(' ')[5]}`);
+        synth.speak(utterThis);
+        return;
+      }
+      utterThis = new SpeechSynthesisUtterance(`the weather condition in ${weather.name} is mostly full of ${weather.weather[0].description} at a temperature of ${weather.main.temp} degrees Celcius`);
+      synth.speak(utterThis);
+    });
+  };
+
 // create a p and append it to div.words
 let p = document.createElement('p');
 const words = document.querySelector('.words');
@@ -18,11 +60,21 @@ recognition.addEventListener('result', e => {
         .join(''); // join all strings
 
         p.textContent = transcript; // types it in the DOM
+
         if(e.results[0].isFinal) { //  boolean that states whether this result is final
             p = document.createElement('p');
-            words.appendChild(p);
+            words.appendChild(p); 
+            if (transcript.includes('what is the time')) {
+                speak(getTime);
+            }; 
+            if (transcript.includes('what is today\'s date')) {
+                speak(getDate);
+            }; 
+            if (transcript.includes('what is the weather in')) {
+                getTheWeather(transcript);
+            };   
         }
-        // console.log(transcript);
+        // console.log(transcript);      
 });
 
 // add another listener to fire when you continue speaking
